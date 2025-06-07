84191175d98d54df2ffae765ac525503903d5f41
Francesco <francesco.209@gmail.com>
2025-06-07 12:39:23 +0200

    ✨ 2016 haskell day01 - 1

---
+++ 2016/day01-1.hs
@@ -0,0 +1,46 @@
+import System.IO
+
+data Direction = N | S | W | E
+data Turn = L | R
+type Position = (Int, Int, Direction)
+type Instruction = (Turn, Int)
+
+solution :: String -> String
+solution content = distance (helper (0, 0, N) [parseOne line | line <- words content])
+    where
+        helper current [] = current
+        helper current (x:xs) = helper (move current x) xs
+
+distance :: Position -> String
+distance (x, y, _) = show (abs x + abs y)
+
+parseOne :: String -> Instruction
+parseOne (x:xs) = (toTurn x, read [d | d <- xs, d `elem` "1234567890"] :: Int)
+    where
+        toTurn 'L' = L
+        toTurn 'R' = R
+
+move :: Position -> Instruction -> Position
+move current instruction = walk (turn current instruction) instruction
+
+walk :: Position -> Instruction -> Position
+walk (x, y, N) (_, b) = (x, y+b, N)
+walk (x, y, S) (_, b) = (x, y-b, S)
+walk (x, y, E) (_, b) = (x+b, y, E)
+walk (x, y, W) (_, b) = (x-b, y, W)
+
+turn :: Position -> Instruction -> Position
+turn (x, y, N) (L, _) = (x, y, W)
+turn (x, y, N) (R, _) = (x, y, E)
+turn (x, y, S) (L, _) = (x, y, E)
+turn (x, y, S) (R, _) = (x, y, W)
+turn (x, y, W) (L, _) = (x, y, S)
+turn (x, y, W) (R, _) = (x, y, N)
+turn (x, y, E) (L, _) = (x, y, N)
+turn (x, y, E) (R, _) = (x, y, S)
+
+main :: IO ()
+main = do
+    handle <- openFile "day01.dat" ReadMode
+    content <- hGetContents handle
+    print . solution $ content
