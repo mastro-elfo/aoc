594c4c53bdd11a5923eb8be0bd4a71d9648e7c1f
Francesco <francesco.209@gmail.com>
2025-06-11 16:26:06 +0200

    ✨ 2022 python day01 - 1,2

---
+++ 2022/day01_2.py
@@ -0,0 +1,23 @@
+# pylint: disable=missing-module-docstring, missing-function-docstring
+
+
+from typing import Any
+
+
+def solution(content: list[str]) -> Any:
+    elves: list[list[int]] = [[]]
+    for line in content:
+        if line.strip() == "":
+            elves += [[]]
+        else:
+            elves[-1] += [int(line)]
+    return sum(sorted([sum(elf) for elf in elves], reverse=True)[:3])
+
+
+def main() -> None:
+    with open("day01.dat", "r", encoding="utf8") as file:
+        print(solution(file.readlines()))
+
+
+if __name__ == "__main__":
+    main()
