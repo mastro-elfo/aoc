1fb27548c3c6eb0ab34d7a04ebeffac41f599f8a
Francesco <francesco.209@gmail.com>
2025-06-17 13:15:15 +0200

    ⚡️ 2018 typescript day02 - 1

--- 2018/day02-1.ts
+++ 2018/day02-1.ts
@@ -1,21 +1,15 @@
 function solution(content: string) {
-  const count = content.split("\n").map((line) => [has2x(line), has3x(line)]);
+  const count = content
+    .split("\n")
+    .map((line) => [hasnx(2, line), hasnx(3, line)]);
   return count.filter(([x]) => x).length * count.filter(([, x]) => x).length;
 }
 
-function has2x(line: string) {
+function hasnx(n: number, line: string) {
   return line
     .split("")
     .map((y) => line.split("").filter((x) => x == y).length)
-    .filter((z) => z == 2)
-    .some((t) => t);
-}
-
-function has3x(line: string) {
-  return line
-    .split("")
-    .map((y) => line.split("").filter((x) => x == y).length)
-    .filter((z) => z == 3)
+    .filter((z) => z == n)
     .some((t) => t);
 }
 
