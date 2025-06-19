38c10be9f75a317ea27e15c2330152614aa677cd
Francesco <francesco.209@gmail.com>
2025-06-19 13:03:25 +0200

    ✨ 2020 haskell day02 - 1,2

---
+++ 2020/day02-1.hs
@@ -0,0 +1,35 @@
+import System.IO
+
+type Entity = (Int, Int, Char, String)
+
+solution :: String -> Int
+solution content = length . filter isValid . map parse . lines $ content
+
+isValid :: Entity -> Bool
+isValid (min, max, char, password) = min <= lng && lng <= max
+    where
+        lng = length . filter (\ch -> ch == char) $ password
+
+parse :: String -> Entity
+parse line = (
+    read . head . splitOn '-' $ line,
+    read . head . splitOn ' ' . head . drop 1 . splitOn '-' $ line,
+    head . head . splitOn ':' . head . drop 1 . splitOn ' ' $ line,
+    last . splitOn ' ' $ line)
+
+splitOn :: Char -> String -> [String]
+splitOn c xs = helper [""] c xs
+    where
+        helper o c "" = o
+        helper o c (x:"")
+            | x == c = o
+            | otherwise = (init o) ++ [(last o) ++ [x]]
+        helper o c (x:xs)
+            | x == c = helper (o++[""]) c xs
+            | otherwise = helper ((init o) ++ [(last o) ++ [x]]) c xs
+
+main :: IO ()
+main = do
+    handle <- openFile "day02.dat" ReadMode
+    content <- hGetContents handle
+    print . solution $ content
