3d4fda1c73a4e7814d37832aad2e4d60b8aa14be
Francesco <francesco.209@gmail.com>
2025-06-20 22:06:03 +0200

    ⚡️ 2022 python day02 - 2

--- 2022/day02_2.py
+++ 2022/day02_2.py
@@ -10,16 +10,39 @@ def solution(content: list[str]) -> Any:
 
 def play(hands: tuple[str, str]):
     opponent, player = hands
-    return (0 if player == "X" else 3 if player == "Y" else 6) + (
-        1
-        if (opponent, player) in [("P", "X"), ("R", "Y"), ("S", "Z")]
-        else 2 if (opponent, player) in [("S", "X"), ("P", "Y"), ("R", "Z")] else 3
+    return (1 if player == "R" else 2 if player == "P" else 3) + (
+        0
+        if (opponent, player) in [("R", "S"), ("S", "P"), ("P", "R")]
+        else 3 if opponent == player else 6
     )
 
 
 def parse(line: str):
     opponent, player = line.strip().split(" ")
-    return ("R" if opponent == "A" else "P" if opponent == "B" else "S", player)
+    return (
+        "R" if opponent == "A" else "P" if opponent == "B" else "S",
+        (
+            "R"
+            if is_rock(opponent, player)
+            else "P" if is_paper(opponent, player) else "S"
+        ),
+    )
+
+
+def is_rock(opponent: str, player):
+    return (
+        (opponent == "B" and player == "X")
+        or (opponent == "A" and player == "Y")
+        or (opponent == "C" and player == "Z")
+    )
+
+
+def is_paper(opponent: str, player):
+    return (
+        (opponent == "C" and player == "X")
+        or (opponent == "B" and player == "Y")
+        or (opponent == "A" and player == "Z")
+    )
 
 
 def main() -> None:
