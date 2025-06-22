4549b527e6d28c5b96a78e40c08d2803c63c91a8
Francesco <francesco.209@gmail.com>
2025-06-22 09:44:07 +0200

    ✨ 2015 typescript day03 - 1,2

---
+++ 2015/day03-1.ts
@@ -0,0 +1,32 @@
+type Position = {
+  x: number;
+  y: number;
+};
+
+function solution(content: string) {
+  return content
+    .split("")
+    .reduce(
+      (acc, direction) => [...acc, move(acc.at(-1)!, direction)],
+      [{ x: 0, y: 0 }]
+    )
+    .filter(
+      (item, index, array) =>
+        !array
+          .slice(0, index)
+          .find((itm) => itm.x === item.x && itm.y === item.y)
+    ).length;
+}
+
+function move({ x, y }: Position, direction: string) {
+  if (direction === "^") return { x, y: y + 1 };
+  if (direction === "v") return { x, y: y - 1 };
+  if (direction === "<") return { x: x - 1, y };
+  if (direction === ">") return { x: x + 1, y };
+  throw new Error(`Invalid direction ${direction}`);
+}
+
+Deno.readTextFile("day03.dat")
+  .then((content) => content.trim())
+  .then(solution)
+  .then(console.log);
