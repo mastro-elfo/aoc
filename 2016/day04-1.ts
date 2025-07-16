c512fceea25ad648402853e7a080e1052019e9f3
Francesco <francesco.209@gmail.com>
2025-07-16 09:23:22 +0200

    ✨ 2016 typescript day03 - 1, 2

---
+++ 2016/day04-1.ts
@@ -0,0 +1,49 @@
+type Room = {
+  name: string;
+  code: number;
+  checksum: string;
+};
+
+function solution(content: string) {
+  return content
+    .split("\n")
+    .map(parse)
+    .filter((room) => checksum(room.name) === room.checksum)
+    .map((room) => room.code)
+    .reduce((acc, cur) => acc + cur, 0);
+}
+
+function checksum(name: string) {
+  function helper(count: Record<string, number>, line: string) {
+    if (line.length === 0) {
+      return Object.entries(count)
+        .toSorted(
+          ([charA], [charB]) => charA.charCodeAt(0) - charB.charCodeAt(0)
+        )
+        .toSorted(([_A, countA], [_B, countB]) => countB - countA)
+        .map(([char]) => char)
+        .join("")
+        .slice(0, 5);
+    }
+    const char = line.at(0)!;
+    const rest = line.slice(1);
+    count[char] = 1 + (count[char] ?? 0);
+    return helper(count, rest);
+  }
+  return helper({}, name.replaceAll("-", ""));
+}
+
+function parse(line: string): Room {
+  const match = line.match(/([a-z\-]+)(\d+)\[([a-z]+)\]/);
+  if (!match) throw new Error(`Invalid line: ${line}`);
+  return {
+    checksum: match.at(3)!,
+    code: Number(match.at(2)),
+    name: match.at(1)!,
+  };
+}
+
+Deno.readTextFile("day04.dat")
+  .then((content) => content.trim())
+  .then(solution)
+  .then(console.log);
