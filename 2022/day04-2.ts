56fe3cc369f8b5f2125005f269eec1fdf4832408
Francesco <francesco.209@gmail.com>
2025-08-07 12:40:01 +0200

    ✨ 2022 typescript day04 - 1, 2

---
+++ 2022/day04-2.ts
@@ -0,0 +1,38 @@
+type Limits = { start: number; end: number };
+type Pair = { first: Limits; second: Limits };
+
+function solution(content: string) {
+  return content.split("\n").map(parse).filter(overlap).length;
+}
+
+function overlap({
+  first: { start: fs, end: fe },
+  second: { start: ss, end: se },
+}: Pair): boolean {
+  return (
+    (ss <= fs && fs <= se) ||
+    (ss <= fe && fe <= se) ||
+    (fs <= ss && ss <= fe) ||
+    (fs <= se && se <= fe)
+  );
+}
+
+function parse(line: string): Pair {
+  const match = line.match(/(\d+)-(\d+),(\d+)-(\d+)/);
+  if (!match) throw new Error(`Invalid line: ${line}`);
+  return {
+    first: {
+      start: Number(match[1]),
+      end: Number(match[2]),
+    },
+    second: {
+      start: Number(match[3]),
+      end: Number(match[4]),
+    },
+  };
+}
+
+Deno.readTextFile("day04.dat")
+  .then((content) => content.trim())
+  .then(solution)
+  .then(console.log);
