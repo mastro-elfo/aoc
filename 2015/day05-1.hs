10fc85557c8f38441fbb335ee4927995d3e9d0c0
Francesco <francesco.209@gmail.com>
2025-08-17 09:48:51 +0200

    ✨ 2015 haskell day04 - 1, 2

---
+++ 2015/day05-1.hs
@@ -0,0 +1,24 @@
+import Data.List (isInfixOf)
+
+
+solution :: String -> Int
+solution =  length . filter isNice . lines
+
+isNice :: String -> Bool
+isNice xs = all id (map (\f -> f xs) [hasDoubledLetter, hasMoreThanThreeVowels, hasNoNaugtyStrings])
+
+hasMoreThanThreeVowels :: String -> Bool
+hasMoreThanThreeVowels = (>=3) . length . filter (`elem` "aeiou")
+
+hasNoNaugtyStrings :: String -> Bool
+hasNoNaugtyStrings xs = all (\x -> not (isInfixOf x xs)) ["ab", "cd", "pq", "xy"]
+
+hasDoubledLetter :: String -> Bool
+hasDoubledLetter "" = False
+hasDoubledLetter (x:"") = False
+hasDoubledLetter (x:y:xs)
+    | x == y = True
+    | otherwise = hasDoubledLetter (y:xs)
+
+main :: IO ()
+main = do readFile "day05.dat" >>= print . solution
---
+++ 2015/day05-2.hs
@@ -0,0 +1,22 @@
+import Data.List (isInfixOf)
+
+solution :: String -> Int
+solution = length . filter isNice . lines
+
+isNice :: String -> Bool
+isNice xs = all id (map (\f -> f xs) [hasRepeatingDouble, hasEfeRule])
+
+hasEfeRule :: String -> Bool
+hasEfeRule (x:y:z:xs)
+    | x == z = True
+    | otherwise = hasEfeRule (y:z:xs)
+hasEfeRule _ = False
+
+hasRepeatingDouble :: String -> Bool
+hasRepeatingDouble (x:y:xs)
+    | isInfixOf (x:y:[]) xs = True
+    | otherwise = hasRepeatingDouble (y:xs)
+hasRepeatingDouble _ = False
+
+main :: IO ()
+main = do readFile "day05.dat" >>= print . solution
