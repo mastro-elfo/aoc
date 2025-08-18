f348b2fd1aef183ff345d1d58b6b3988ed0f4487
Francesco <francesco.209@gmail.com>
2025-08-18 12:18:11 +0200

    ♻️ Fix function name "naugthy" + minor refactor

--- 2015/day05-1.hs
+++ 2015/day05-1.hs
@@ -5,20 +5,19 @@ solution :: String -> Int
 solution =  length . filter isNice . lines
 
 isNice :: String -> Bool
-isNice xs = all (\f -> f xs) [hasDoubledLetter, hasMoreThanThreeVowels, hasNoNaugtyStrings]
+isNice xs = all (\f -> f xs) [hasDoubledLetter, hasMoreThanThreeVowels, hasNoNaughtyStrings]
 
 hasMoreThanThreeVowels :: String -> Bool
 hasMoreThanThreeVowels = (>=3) . length . filter (`elem` "aeiou")
 
-hasNoNaugtyStrings :: String -> Bool
-hasNoNaugtyStrings xs = all (\x -> not (isInfixOf x xs)) ["ab", "cd", "pq", "xy"]
+hasNoNaughtyStrings :: String -> Bool
+hasNoNaughtyStrings xs = all (\x -> not (isInfixOf x xs)) ["ab", "cd", "pq", "xy"]
 
 hasDoubledLetter :: String -> Bool
-hasDoubledLetter "" = False
-hasDoubledLetter (x:"") = False
 hasDoubledLetter (x:y:xs)
     | x == y = True
     | otherwise = hasDoubledLetter (y:xs)
+hasDoubledLetter _ = False
 
 main :: IO ()
 main = do readFile "day05.dat" >>= print . solution
