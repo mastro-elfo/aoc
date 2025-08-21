ef283dded8f3d72985c93d005047ddc3d6fd2521
Francesco <francesco.209@gmail.com>
2025-08-21 09:43:08 +0200

    ✨ 2015 typescript day04 - 1, 2

---
+++ 2015/day04-2.ts
@@ -0,0 +1,18 @@
+import { md5 } from "@takker/md5";
+import { encodeHex } from "jsr:@std/encoding@1/hex";
+
+function solution(content: string) {
+  let index = 1;
+  while (true) {
+    const hashed = md5(content + index);
+    if (encodeHex(hashed).startsWith("000000")) {
+      return index;
+    }
+    index += 1;
+  }
+}
+
+Deno.readTextFile("day04.dat")
+  .then((content) => content.trim())
+  .then(solution)
+  .then(console.log);
