815fa7f24054d49de3191c6d81ff6566b1315c5c
Francesco <francesco.209@gmail.com>
2025-08-23 10:34:00 +0200

    ✨ 2017 python day05 - 2

---
+++ 2017/day05_2.py
@@ -0,0 +1,29 @@
+# pylint: disable=missing-module-docstring, missing-function-docstring
+
+
+from typing import Any
+
+
+def solution(content: list[str]) -> Any:
+    return play([int(j) for j in content])
+
+
+def play(jumps: list[int]):
+    step = 0
+    index = 0
+    copy = jumps[::]
+    while 0 <= index < len(copy):
+        offset = copy[index]
+        copy[index] += 1 if copy[index] < 3 else -1
+        index += offset
+        step += 1
+    return step
+
+
+def main() -> None:
+    with open("day05.dat", "r", encoding="utf8") as file:
+        print(solution(file.readlines()))
+
+
+if __name__ == "__main__":
+    main()
