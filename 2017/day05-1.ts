e7d643c9cbeed0271a1a847babd36335ec48bd56
Francesco <francesco.209@gmail.com>
2025-08-24 09:28:19 +0200

    ✨ 2017 typescript day05 - 1, 2

---
+++ 2017/day05-1.ts
@@ -0,0 +1,21 @@
+function solution(content: string) {
+  return play(content.split("\n").map(Number));
+}
+
+function play(jumps: number[]) {
+  let step = 0;
+  let index = 0;
+  const copy = jumps.slice();
+  while (0 <= index && index < copy.length) {
+    const offset = copy[index];
+    copy[index] += 1;
+    index += offset;
+    step += 1;
+  }
+  return step;
+}
+
+Deno.readTextFile("day05.dat")
+  .then((content) => content.trim())
+  .then(solution)
+  .then(console.log);
