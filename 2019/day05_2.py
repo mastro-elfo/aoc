0c9ed44ae0400fdb20cfdfe8368ae4a96122b42a
Francesco <francesco.209@gmail.com>
2025-08-29 12:48:28 +0200

    ✨ 2019 python day05 - 1, 2

---
+++ 2019/day05_2.py
@@ -0,0 +1,84 @@
+# pylint: disable=missing-module-docstring, missing-function-docstring
+
+
+from typing import Any
+
+
+def solution(content: str) -> Any:
+    program = [int(x) for x in content.split(",")]
+    current = 0
+    halt = False
+    output = None
+    while not halt:
+        current, program, halt, n_output = execute(current, program)
+        output = n_output if n_output is not None else output
+    return output
+
+
+def execute(current: int, program: list[int]):
+    current_digits = str(program[current]).rjust(5, "0")
+    fst_mod = current_digits[2]
+    snd_mod = current_digits[1]
+    opcode = current_digits[3] + current_digits[4]
+
+    if opcode not in ["01", "02", "03", "04", "05", "06", "07", "08", "99"]:
+        raise ValueError(f"Invalid opcode {opcode} at {current} in {program}")
+
+    copy = program.copy()
+    if opcode == "01":
+        copy[copy[current + 3]] = get_value(program, current, 1, fst_mod) + get_value(
+            program, current, 2, snd_mod
+        )
+        return (current + 4, copy, False, None)
+    if opcode == "02":
+        copy[copy[current + 3]] = get_value(program, current, 1, fst_mod) * get_value(
+            program, current, 2, snd_mod
+        )
+        return (current + 4, copy, False, None)
+    if opcode == "03":
+        copy[copy[current + 1]] = 5
+        return (current + 2, copy, False, None)
+    if opcode == "04":
+        copy[copy[current + 1]] = get_value(program, current, 1, fst_mod)
+        return (current + 2, copy, False, copy[copy[current + 1]])
+    if opcode == "05":
+        if get_value(program, current, 1, fst_mod) != 0:
+            return (get_value(program, current, 2, snd_mod), copy, False, None)
+        return (current + 3, copy, False, None)
+    if opcode == "06":
+        if get_value(program, current, 1, fst_mod) == 0:
+            return (get_value(program, current, 2, snd_mod), copy, False, None)
+        return (current + 3, copy, False, None)
+    if opcode == "07":
+        copy[copy[current + 3]] = (
+            1
+            if get_value(program, current, 1, fst_mod)
+            < get_value(program, current, 2, snd_mod)
+            else 0
+        )
+        return (current + 4, copy, False, None)
+    if opcode == "08":
+        copy[copy[current + 3]] = (
+            1
+            if get_value(program, current, 1, fst_mod)
+            == get_value(program, current, 2, snd_mod)
+            else 0
+        )
+        return (current + 4, copy, False, None)
+    # if opcode == "99":
+    return (current, copy, True, None)
+
+
+def get_value(program: list[int], current: int, param: int, mode: str):
+    if mode == "1":
+        return program[current + param]
+    return program[program[current + param]]
+
+
+def main() -> None:
+    with open("day05.dat", "r", encoding="utf8") as file:
+        print(solution(file.read().strip()))
+
+
+if __name__ == "__main__":
+    main()
