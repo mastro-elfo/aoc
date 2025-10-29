4cbd3d9cbd9a93b92037d5dc6dea7df403de91c3
Francesco <francesco.209@gmail.com>
2025-10-29 12:56:53 +0100

    ♻️ 2023 haskell day06 - 1

--- 2023/day06-1.hs
+++ 2023/day06-1.hs
@@ -1,10 +1,14 @@
 import Debug.Trace (trace)
 
 solution :: String -> Int
-solution = product . map (uncurry winning) . parse
+solution = product . map winning . parse
 
-winning :: Int -> Int -> Int
-winning t d = length . filter id . map (\x -> d < x * (t - x)) $ [0 .. t]
+winning :: (Int, Int) -> Int
+winning (t, d) = floor (halfT + delta) - ceiling (halfT - delta) + 1 - if' 2 0 (isInt intersect)
+  where
+    delta = (sqrt . fromIntegral) (t * t - 4 * d) / 2
+    halfT = fromIntegral t / 2
+    intersect = halfT + delta
 
 parse :: String -> [(Int, Int)]
 parse content = zip times dists
@@ -12,6 +16,13 @@ parse content = zip times dists
     lns = lines content
     times = map read . tail . words . head $ lns
     dists = map read . tail . words . last $ lns
+
+if' :: a -> a -> Bool -> a
+if' a _ True = a
+if' _ a False = a
+
+isInt :: (RealFrac a) => a -> Bool
+isInt x = ceiling x == floor x
 
 main :: IO ()
 main = do readFile "day06.dat" >>= print . solution
