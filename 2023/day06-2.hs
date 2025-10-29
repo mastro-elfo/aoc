c9e2026258b31beea1afa5386421d3fe9612b088
Francesco <francesco.209@gmail.com>
2025-10-29 13:00:44 +0100

    ✨ 2023 haskell day06 - 2

---
+++ 2023/day06-2.hs
@@ -0,0 +1,28 @@
+import Debug.Trace (trace)
+
+solution :: String -> Int
+solution = winning . parse
+
+winning :: (Int, Int) -> Int
+winning (t, d) = floor (halfT + delta) - ceiling (halfT - delta) + 1 - if' 2 0 (isInt intersect)
+  where
+    delta = (sqrt . fromIntegral) (t * t - 4 * d) / 2
+    halfT = fromIntegral t / 2
+    intersect = halfT + delta
+
+parse :: String -> (Int, Int)
+parse content = ((read . concat) times, (read . concat) dists)
+  where
+    lns = lines content
+    times = tail . words . head $ lns
+    dists = tail . words . last $ lns
+
+if' :: a -> a -> Bool -> a
+if' a _ True = a
+if' _ a False = a
+
+isInt :: (RealFrac a) => a -> Bool
+isInt x = ceiling x == floor x
+
+main :: IO ()
+main = do readFile "day06.dat" >>= print . solution
