74d8e7b2d31374d5b77907b005032f768087d609
Francesco <francesco.209@gmail.com>
2025-11-10 08:57:46 +0100

    ♻️ 2016 haskell day01 - 1

--- 2016/day01-1.hs
+++ 2016/day01-1.hs
@@ -1,33 +1,36 @@
 import System.IO
 
 data Direction = N | S | W | E
+
 data Turn = L | R
+
 type Position = (Int, Int, Direction)
+
 type Instruction = (Turn, Int)
 
-solution :: String -> String
+solution :: String -> Int
 solution content = distance (helper (0, 0, N) [parseOne line | line <- words content])
-    where
-        helper current [] = current
-        helper current (x:xs) = helper (move current x) xs
+  where
+    helper current [] = current
+    helper current (x : xs) = helper (move current x) xs
 
-distance :: Position -> String
-distance (x, y, _) = show (abs x + abs y)
+distance :: Position -> Int
+distance (x, y, _) = abs x + abs y
 
 parseOne :: String -> Instruction
-parseOne (x:xs) = (toTurn x, read [d | d <- xs, d `elem` "1234567890"] :: Int)
-    where
-        toTurn 'L' = L
-        toTurn 'R' = R
+parseOne (x : xs) = (toTurn x, read [d | d <- xs, d `elem` "1234567890"] :: Int)
+  where
+    toTurn 'L' = L
+    toTurn 'R' = R
 
 move :: Position -> Instruction -> Position
 move current instruction = walk (turn current instruction) instruction
 
 walk :: Position -> Instruction -> Position
-walk (x, y, N) (_, b) = (x, y+b, N)
-walk (x, y, S) (_, b) = (x, y-b, S)
-walk (x, y, E) (_, b) = (x+b, y, E)
-walk (x, y, W) (_, b) = (x-b, y, W)
+walk (x, y, N) (_, b) = (x, y + b, N)
+walk (x, y, S) (_, b) = (x, y - b, S)
+walk (x, y, E) (_, b) = (x + b, y, E)
+walk (x, y, W) (_, b) = (x - b, y, W)
 
 turn :: Position -> Instruction -> Position
 turn (x, y, N) (L, _) = (x, y, W)
@@ -40,7 +43,4 @@ turn (x, y, E) (L, _) = (x, y, N)
 turn (x, y, E) (R, _) = (x, y, S)
 
 main :: IO ()
-main = do
-    handle <- openFile "day01.dat" ReadMode
-    content <- hGetContents handle
-    print . solution $ content
+main = do readFile "day01.dat" >>= print . solution
