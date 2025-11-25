13dcb06209b8ea3c29d2913ce38dc130876bfe21
Francesco <francesco.209@gmail.com>
2025-11-25 09:31:41 +0100

    ✨ 2021 typescript day07 - 1, 2

---
+++ 2021/day07-1.ts
@@ -0,0 +1,25 @@
+function solution(content: string) {
+  const positions = content.split(",").map(Number);
+  const minPosition = Math.min(...positions);
+  const maxPosition = Math.max(...positions);
+  return Math.min(
+    ...Array.from({ length: maxPosition - minPosition })
+      .map((_, index) => minPosition + index)
+      .map((end) => fuelTot(positions, end))
+  );
+}
+
+function fuelTo(start: number, end: number): number {
+  return Math.abs(start - end);
+}
+
+function fuelTot(starts: number[], end: number): number {
+  return starts
+    .map((start) => fuelTo(start, end))
+    .reduce((acc, cur) => acc + cur, 0);
+}
+
+Deno.readTextFile("day07.dat")
+  .then((content) => content.trim())
+  .then(solution)
+  .then(console.log);
