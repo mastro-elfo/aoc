1a72f6fb29742fdc2e19bc35914c534eef3e86cf
Francesco <francesco.209@gmail.com>
2025-12-05 12:04:40 +0100

    ♻️ 2025 haskell day04 - 1, 2

--- 2025/day04-1.hs
+++ 2025/day04-1.hs
@@ -4,12 +4,12 @@ import Data.Map.Strict (member)
 type Paper = (Int, Int)
 
 solution :: String -> Int
-solution content = length . Data.Map.keys . Data.Map.filter (< 4) . Data.Map.mapWithKey (\k _ -> length . neighbors papers $ k) $ papers
+solution content = length . Data.Map.filter (< 4) . Data.Map.mapWithKey (\k _ -> length . neighbors papers $ k) $ papers
   where
     papers = parse (0, 0) content
 
 neighbors :: Data.Map.Map Paper Int -> Paper -> [Paper]
-neighbors mp (x, y) = Prelude.filter (`member` mp) [(x - 1, y), (x + 1, y), (x, y - 1), (x, y + 1), (x - 1, y - 1), (x - 1, y + 1), (x + 1, y - 1), (x + 1, y + 1)]
+neighbors mp (x, y) = filter (`member` mp) [(x - 1, y), (x + 1, y), (x, y - 1), (x, y + 1), (x - 1, y - 1), (x - 1, y + 1), (x + 1, y - 1), (x + 1, y + 1)]
 
 parse :: Paper -> String -> Data.Map.Map Paper Int
 parse _ [] = Data.Map.empty
--- 2025/day04-2.hs
+++ 2025/day04-2.hs
@@ -16,7 +16,7 @@ remove papers
     filtered = Data.Map.filter (>= 4) . Data.Map.mapWithKey (\k _ -> length . neighbors papers $ k) $ papers
 
 neighbors :: Data.Map.Map Paper Int -> Paper -> [Paper]
-neighbors mp (x, y) = Prelude.filter (`member` mp) [(x - 1, y), (x + 1, y), (x, y - 1), (x, y + 1), (x - 1, y - 1), (x - 1, y + 1), (x + 1, y - 1), (x + 1, y + 1)]
+neighbors mp (x, y) = filter (`member` mp) [(x - 1, y), (x + 1, y), (x, y - 1), (x, y + 1), (x - 1, y - 1), (x - 1, y + 1), (x + 1, y - 1), (x + 1, y + 1)]
 
 parse :: Paper -> String -> Data.Map.Map Paper Int
 parse _ [] = Data.Map.empty
