54c99d27b94fd50a868562c4c2e609ef96349d18
Francesco <francesco.209@gmail.com>
2025-12-18 15:50:49 +0100

    ✨ 2015 haskell day04 - 1, 2

---
+++ 2015/day04-1.hs
@@ -0,0 +1,15 @@
+import Data.ByteString.Char8 (pack)
+import Data.List (isPrefixOf)
+import Distribution.Utils.MD5 (md5)
+
+solution :: String -> Int
+solution = helper 1
+  where
+    helper index content
+      | "00000" `isPrefixOf` hashed = index
+      | otherwise = helper (index + 1) content
+      where
+        hashed = show . md5 . pack $ (content ++ show index)
+
+main :: IO ()
+main = do readFile "day04.dat" >>= print . solution
