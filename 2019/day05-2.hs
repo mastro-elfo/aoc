07ac2e61fb1318b061f18e99086637c718e1cb81
Francesco <francesco.209@gmail.com>
2026-01-09 12:27:47 +0100

    ✨ 2019 haskell day05 - 1, 2

---
+++ 2019/day05-2.hs
@@ -0,0 +1,61 @@
+{-# OPTIONS_GHC -Wno-x-partial -Wno-unrecognised-warning-flags #-}
+
+data Mode = P | I
+
+type Program = [Int]
+
+solution :: String -> Int
+solution = execute [5] 0 0 . parse
+
+execute :: [Int] -> Int -> Int -> Program -> Int
+execute ins ous cur prg
+  | op == "99" = ous
+  | op == "01" = execute ins ous (cur + 4) (substitute (position 3 cur prg) (value mod1 1 cur prg + value mod2 2 cur prg) prg)
+  | op == "02" = execute ins ous (cur + 4) (substitute (position 3 cur prg) (value mod1 1 cur prg * value mod2 2 cur prg) prg)
+  | op == "03" = execute (tail ins) ous (cur + 2) (substitute (position 1 cur prg) (head ins) prg)
+  | op == "04" = execute ins (value mod1 1 cur prg) (cur + 2) prg
+  | op == "05" = execute ins ous (tf (value mod2 2 cur prg) (cur + 3) (value mod1 1 cur prg /= 0)) prg
+  | op == "06" = execute ins ous (tf (value mod2 2 cur prg) (cur + 3) (value mod1 1 cur prg == 0)) prg
+  | op == "07" = execute ins ous (cur + 4) (substitute (position 3 cur prg) (tf 1 0 (value mod1 1 cur prg < value mod2 2 cur prg)) prg)
+  | op == "08" = execute ins ous (cur + 4) (substitute (position 3 cur prg) (tf 1 0 (value mod1 1 cur prg == value mod2 2 cur prg)) prg)
+  | otherwise = 0
+  where
+    (mod3, mod2, mod1, op) = parseOpCode . left50pad . show . (!! cur) $ prg
+
+tf :: a -> a -> Bool -> a
+tf a _ True = a
+tf _ a False = a
+
+substitute :: Int -> Int -> Program -> Program
+substitute pos val prg = take pos prg ++ [val] ++ drop (pos + 1) prg
+
+value :: Mode -> Int -> Int -> Program -> Int
+value I n cur prg = (!! (n + cur)) prg
+value P n cur prg = (!! (!! (n + cur)) prg) prg
+
+position :: Int -> Int -> Program -> Int
+position n cur = (!! (n + cur))
+
+parseOpCode :: String -> (Mode, Mode, Mode, String)
+parseOpCode xs =
+  ( char2mode . (!! 0) $ xs,
+    char2mode . (!! 1) $ xs,
+    char2mode . (!! 2) $ xs,
+    take 2 . drop 3 $ xs
+  )
+
+char2mode :: Char -> Mode
+char2mode '0' = P
+char2mode '1' = I
+
+left50pad :: String -> String
+left50pad xs
+  | length xs > 4 = xs
+  | otherwise = replicate (5 - length xs) '0' ++ xs
+
+parse :: String -> Program
+parse "" = []
+parse xs = (read . takeWhile (/= ',') $ xs) : parse (tail . dropWhile (/= ',') $ xs)
+
+main :: IO ()
+main = do readFile "day05.dat" >>= print . solution
--- README.md
+++ README.md
@@ -48,7 +48,7 @@ https://adventofcode.com
 - https://adventofcode.com/2019/day/2 ✓✓✓
 - https://adventofcode.com/2019/day/3 ✓✓✓
 - https://adventofcode.com/2019/day/4 ✓✓✓
-- https://adventofcode.com/2019/day/5 ✓✓
+- https://adventofcode.com/2019/day/5 ✓✓✓
 - https://adventofcode.com/2019/day/6 ✓✓✓
 - https://adventofcode.com/2019/day/7 ✓✓✓
 
