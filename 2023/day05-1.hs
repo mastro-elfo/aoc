a2338d57461ae3da1dccf8b36019552e0ea741b7
Francesco <francesco.209@gmail.com>
2026-01-12 16:52:42 +0100

    ♻️ 2023 haskell day05 - 1

--- 2023/day05-1.hs
+++ 2023/day05-1.hs
@@ -1,30 +1,33 @@
+{-# OPTIONS_GHC -Wno-x-partial -Wno-unrecognised-warning-flags #-}
+
 type Table = (Int, Int, Int)
 
 solution :: String -> Int
 solution content = minimum . map (flip (foldl convert) $ maps) $ seeds
-    where
-        seeds :: [Int] = map read . words . dropWhile (/= ' ') . head . lines $ content
-        maps :: [[Table]] = parseMaps . drop 2 . lines $ content
+  where
+    seeds :: [Int] = map read . words . dropWhile (/= ' ') . head . lines $ content
+    maps :: [[Table]] = parseMaps . drop 2 . lines $ content
 
 parseMaps :: [String] -> [[Table]]
-parseMaps content = helper [] content
-    where
-        helper :: [[Table]] -> [String] -> [[Table]]
-        helper ts [] = ts
-        helper ts ("":xs) = helper ts xs
-        helper ts (x:xs)
-            | elem '-' x = helper (ts ++ [[]]) xs
-            | otherwise = helper ((init ts) ++ [last ts ++ [parseTable x]]) xs
+parseMaps = helper []
+  where
+    helper :: [[Table]] -> [String] -> [[Table]]
+    helper ts [] = ts
+    helper ts ("" : xs) = helper ts xs
+    helper ts (x : xs)
+      | '-' `elem` x = helper (ts ++ [[]]) xs
+      | otherwise = helper (init ts ++ [last ts ++ [parseTable x]]) xs
 
 parseTable :: String -> Table
-parseTable line = (values !! 0, values !! 1, values !! 2)
-    where values :: [Int] = map read . words $ line
+parseTable line = (head values, values !! 1, values !! 2)
+  where
+    values :: [Int] = map read . words $ line
 
 convert :: Int -> [Table] -> Int
 convert v [] = v
-convert v (t:ts)
-    | isIn t v = conversion t v
-    | otherwise = convert v ts
+convert v (t : ts)
+  | isIn t v = conversion t v
+  | otherwise = convert v ts
 
 conversion :: Table -> Int -> Int
 conversion (dst, src, _) v = dst + v - src
