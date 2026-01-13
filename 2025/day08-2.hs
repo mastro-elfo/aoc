4a6087f052560cc81be69a17ee311919d2f77b9a
Francesco <francesco.209@gmail.com>
2026-01-13 10:51:57 +0100

    ♻️ 2025 haskell day08 - 2

--- 2025/day08-2.hs
+++ 2025/day08-2.hs
@@ -1,3 +1,5 @@
+{-# OPTIONS_GHC -Wno-x-partial -Wno-unrecognised-warning-flags #-}
+
 import Data.Function (on)
 import Data.List (find, sortBy)
 import Data.Maybe (fromJust, isJust)
@@ -11,21 +13,39 @@ type Distances = (Coords, Coords, Float)
 
 type Circuit = [Coords]
 
+type Connection = (Coords, Coords)
+
 solution :: String -> Int
-solution content = multiplyX . connect (length boxes) . map (\(a, b, _) -> [a, b]) . sortBy (compare `on` trdOf3) . toDistances $ boxes
+solution content =
+  multiplyX
+    . connect (length boxes) []
+    . map (\(a, b, _) -> (a, b))
+    . sortBy (compare `on` trdOf3)
+    . toDistances
+    $ boxes
   where
     boxes = map parse . lines $ content
 
-connect :: Int -> [Circuit] -> (Coords, Coords)
-connect n (cs : css)
-  | isComplete = (head jOther, last jOther)
-  | isJust other = connect n (unique (cs ++ jOther) : filter (/= jOther) css)
-  | otherwise = connect n (css ++ [cs])
+connect :: Int -> [Circuit] -> [Connection] -> Connection
+connect n cs [d] = d
+connect n cs (d : ds)
+  | (length . head $ newconfig) == n = d
+  | otherwise = connect n newconfig ds
   where
-    other = find (\os -> any (`elem` os) cs) css
-    jOther = fromJust other
-    newConfig = unique (cs ++ jOther) : filter (/= jOther) css
-    isComplete = isJust other && ((length . head $ newConfig) == n)
+    newconfig = merge cs d
+
+merge :: [Circuit] -> Connection -> [Circuit]
+merge cs (box1, box2)
+  | isJust c1 && isJust c2 && justC1 == justC2 = cs
+  | isJust c1 && isJust c2 = unique (justC1 ++ justC2) : filter (\c -> c /= justC1 || c /= justC2) cs
+  | isJust c1 = (box2 : justC1) : filter (/= justC1) cs
+  | isJust c2 = (box1 : justC2) : filter (/= justC2) cs
+  | otherwise = [box1, box2] : cs
+  where
+    c1 = find (\c -> box1 `elem` c) cs
+    c2 = find (\c -> box2 `elem` c) cs
+    justC1 = fromJust c1
+    justC2 = fromJust c2
 
 multiplyX :: (Coords, Coords) -> Int
 multiplyX ((x1, _, _), (x2, _, _)) = x1 * x2
