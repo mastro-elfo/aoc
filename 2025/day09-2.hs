9dbcdd104cf9ebc7822756ad86a56f28544e5aec
Francesco <francesco.209@gmail.com>
2026-01-16 15:32:41 +0100

    ⚡️ 2025 haskell day09 - 2

--- 2025/day09-2.hs
+++ 2025/day09-2.hs
@@ -1,101 +1,100 @@
 import Data.Function (on)
 import Data.List (find, sortBy)
-import Data.Maybe (catMaybes, fromJust)
+import Data.Maybe (fromJust)
 import Data.Ord (Down (Down), comparing)
 import Data.Set (fromList, toList)
-import Distribution.Utils.Generic (trdOf3)
 
 type Coords = (Int, Int)
 
-type Element = (Coords, Coords, Int)
+type Segment = (Coords, Coords)
+
+type Rectangle = (Coords, Coords)
 
 solution :: String -> Int
 solution content =
-  trdOf3
+  snd
     . fromJust
-    . find
-      (isValid reds)
-    -- . filter ((== 1560475800) . trdOf3)
-    . sortBy (comparing Down `on` trdOf3)
-    $ areas
+    . find (isValidRectangle grid . fst)
+    . sortBy (comparing Down `on` snd)
+    . generate
+    $ vertices
   where
-    reds = map parse . lines $ content
-    areas = allAreas reds
+    vertices = map parse . lines $ content
+    sides = zip vertices (drop 1 vertices ++ take 1 vertices)
+    horizontals = filter isHorizontal sides
+    grid =
+      unique
+        . map (\((x1, y1), (x2, y2)) -> ((min x1 x2, min y1 y2), (max x1 x2, max y1 y2)))
+        . filter (not . isInternalRectangle (vertices, sides, horizontals))
+        $ zip vertices (drop 2 vertices ++ take 2 vertices)
 
-isValid :: [Coords] -> Element -> Bool
-isValid reds ((x1, y1), (x2, y2), a) =
+isValidRectangle :: [Rectangle] -> Rectangle -> Bool
+isValidRectangle rs r = not (any (overlap r) rs)
+
+overlap :: Rectangle -> Rectangle -> Bool
+overlap rect1 rect2 = helper rect1 rect2 || helper rect2 rect1
+  where
+    helper rect ((x1, y1), (x2, y2)) =
+      helper2 rect ((x1, y1), (x2, y2))
+        || any
+          (helper1 rect)
+          [(x1, y1), (x1, y2), (x2, y1), (x2, y2)]
+    helper1 ((x1, y1), (x2, y2)) (x, y) = min x1 x2 < x && x < max x1 x2 && min y1 y2 < y && y < max y1 y2
+    helper2 ((a1, b1), (a2, b2)) ((c1, d1), (c2, d2)) = min a1 a2 <= min c1 c2 && max c1 c2 <= max a1 a2 && min d1 d2 <= min b1 b2 && max b1 b2 <= max d1 d2
+
+generate :: [Coords] -> [(Rectangle, Int)]
+generate [] = []
+generate (x : xs) = map (\y -> ((x, y), area (x, y))) xs ++ generate xs
+
+area :: Rectangle -> Int
+area ((x1, y1), (x2, y2)) = (abs (x1 - x2) + 1) * (abs (y1 - y2) + 1)
+
+isInternalRectangle :: ([Coords], [Segment], [Segment]) -> Rectangle -> Bool
+isInternalRectangle dt ((x1, y1), (x2, y2)) =
   all
-    (\item -> isRed reds item || isSideGreen reds item || isInsideGreen reds item)
-    . filter (\(x, y) -> min x1 x2 <= x && x <= max x1 x2 && min y1 y2 <= y && y <= max y1 y2)
-    $ ( (unique . neighbors $ reds)
-          ++ ( neighbors
-                 . catMaybes
-                 $ [ s1 `intersect` s2
-                   | s1 <- zip reds (last reds : init reds),
-                     s2 <- [((x1, y1), (x1, y2)), ((x1, y2), (x2, y2)), ((x2, y2), (x2, y1)), ((x2, y1), (x1, y1))]
-                   ]
-             )
-      )
+    (isInternal dt)
+    [(xmin + 1, ymin + 1), (xmax - 1, ymin + 1), (xmax - 1, ymax - 1), (xmin + 1, ymax - 1)]
+  where
+    xmin = min x1 x2
+    xmax = max x1 x2
+    ymin = min y1 y2
+    ymax = max y1 y2
 
-intersect :: (Coords, Coords) -> (Coords, Coords) -> Maybe Coords
-intersect ((a1x, a1y), (a2x, a2y)) ((b1x, b1y), (b2x, b2y))
-  | a1x == a2x && b1y == b2y && isBetween (a1x, b1y) ((a1x, a1y), (a2x, a2y)) && isBetween (a1x, b1y) ((b1x, b1y), (b2x, b2y)) = Just (a1x, b1y)
-  | a1y == a2y && b1x == b2x && isBetween (b1x, a1y) ((a1x, a1y), (a2x, a2y)) && isBetween (b1x, a1y) ((b1x, b1y), (b2x, b2y)) = Just (b1x, a1y)
-  | otherwise = Nothing
-
-isBetween :: Coords -> (Coords, Coords) -> Bool
-isBetween (x, y) ((sx, sy), (ex, ey))
-  | sx == ex && sx == x = min sy ey < y && y < max sy ey
-  | sy == ey && sy == y = min sx ex < x && x < max sx ex
-  | otherwise = False
-
-neighbors :: [Coords] -> [Coords]
-neighbors [] = []
-neighbors ((x, y) : rest) =
-  [ (x - 1, y - 1),
-    (x - 1, y),
-    (x - 1, y + 1),
-    (x, y - 1),
-    (x, y),
-    (x, y + 1),
-    (x + 1, y - 1),
-    (x + 1, y),
-    (x + 1, y + 1)
-  ]
-    ++ neighbors rest
+isInternal :: ([Coords], [Segment], [Segment]) -> Coords -> Bool
+isInternal (vertices, segments, horizontals) coords =
+  isRed vertices coords
+    || isSideGreen segments coords
+    || isInsideGreen horizontals coords
 
 isRed :: [Coords] -> Coords -> Bool
-isRed reds x = x `elem` reds
+isRed vs x = x `elem` vs
 
-isSideGreen :: [Coords] -> Coords -> Bool
-isSideGreen reds (x, y) =
-  any
-    ( \((sx, sy), (ex, ey)) ->
-        (min sx ex <= x && x <= max sx ex && sy == y && y == ey)
-          || (sx == x && x == ex && min sy ey <= y && y <= max sy ey)
-    )
-    $ zip reds (last reds : init reds)
+isSideGreen :: [Segment] -> Coords -> Bool
+isSideGreen [] _ = False
+isSideGreen (segment : xs) (x, y)
+  | isHorizontal segment = y == y1 && isBetween x x1 x2 || isSideGreen xs (x, y)
+  | isVertical segment = x == x1 && isBetween y y1 y2 || isSideGreen xs (x, y)
+  where
+    ((x1, y1), (x2, y2)) = segment
 
-isInsideGreen :: [Coords] -> Coords -> Bool
-isInsideGreen reds (x, y) =
-  (== 1)
-    . (`mod` 2)
-    . length
-    . filter (\((sx, sy), (ex, ey)) -> sy == ey && ey > y && min sx ex <= x && x < max sx ex)
-    $ zip reds (last reds : init reds)
+isInsideGreen :: [Segment] -> Coords -> Bool
+isInsideGreen [] _ = False
+isInsideGreen xs (x, y) = odd . length . filter (\((x1, y1), (x2, _)) -> y1 < y && isBetween x x1 x2) $ xs
 
-allAreas :: [Coords] -> [Element]
-allAreas [] = []
-allAreas (x : xs) = map (\y -> (x, y, area x y)) xs ++ allAreas xs
+isBetween :: Int -> Int -> Int -> Bool
+isBetween x s e = min s e < x && x < max s e
 
-area :: Coords -> Coords -> Int
-area (x1, y1) (x2, y2) = (abs (x1 - x2) + 1) * (abs (y1 - y2) + 1)
+isHorizontal :: Segment -> Bool
+isHorizontal ((_, y1), (_, y2)) = y1 == y2
 
-parse :: String -> Coords
-parse line = (read . takeWhile (/= ',') $ line, read . drop 1 . dropWhile (/= ',') $ line)
+isVertical :: Segment -> Bool
+isVertical ((x1, _), (x2, _)) = x1 == x2
 
 unique :: (Ord a) => [a] -> [a]
 unique = toList . fromList
+
+parse :: String -> Coords
+parse xs = (read . takeWhile (/= ',') $ xs, read . drop 1 . dropWhile (/= ',') $ xs)
 
 main :: IO ()
 main = do readFile "day09.dat" >>= print . solution
