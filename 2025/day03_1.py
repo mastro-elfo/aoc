a990f533c8a4a68b755e8ecbc9ff6f645faa88cc
Francesco <francesco.209@gmail.com>
2026-03-18 17:17:00 +0100

    ✨ 2025 python day03 - 1, 2

---
+++ 2025/day03_1.py
@@ -0,0 +1,37 @@
+# pylint: disable=missing-module-docstring, missing-function-docstring
+
+
+from typing import Any
+
+
+def solution(content: list[str]) -> Any:
+    return sum(to_int(extract(2, parse(line))) for line in content)
+
+
+def to_int(lst: list[int]) -> int:
+    total = 0
+    for item in lst:
+        total = total * 10 + item
+    return total
+
+
+def extract(n: int, joltage: list[int]) -> list[int]:
+    if n == 0:
+        return [0]
+    if n == 1:
+        return [max(joltage)]
+    first = max(joltage[: -n + 1])
+    return [first, *extract(n - 1, joltage[joltage.index(first) + 1 :])]
+
+
+def parse(line: str) -> list[int]:
+    return [int(x) for x in line.strip()]
+
+
+def main() -> None:
+    with open("day03.dat", "r", encoding="utf8") as file:
+        print(solution(file.readlines()))
+
+
+if __name__ == "__main__":
+    main()
--- README.md
+++ README.md
@@ -6,7 +6,7 @@ https://adventofcode.com
 
 - https://adventofcode.com/2025/day/1 ✓✓
 - https://adventofcode.com/2025/day/2 ✓✓✨
-- https://adventofcode.com/2025/day/3 ✓✨
+- https://adventofcode.com/2025/day/3 ✓✓✨
 - https://adventofcode.com/2025/day/4 ✓
 - https://adventofcode.com/2025/day/5 ✓
 - https://adventofcode.com/2025/day/6 ✓
